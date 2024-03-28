107cc80919b35a97cdddb81152effbe5cac07de7
Andrew Morris <voltrevo@gmail.com>
2024-03-28 12:42:09 +1100

    Import compression contracts

--- packages/bundler/contracts/Import.sol
+++ packages/bundler/contracts/Import.sol
@@ -4,3 +4,6 @@ pragma solidity ^0.8.0;
 // import contracts to get their type info.
 import "@account-abstraction/utils/contracts/test/SampleRecipient.sol";
 import "@account-abstraction/contracts/samples/SimpleAccountFactory.sol";
+
+import {HandleOpsCaller} from "./compression/HandleOpsCaller.sol";
+import {HandleAggregatedOpsCaller} from "./compression/HandleAggregatedOpsCaller.sol";
